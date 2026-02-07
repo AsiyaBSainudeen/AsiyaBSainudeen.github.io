e10043e6ccbbfdce671bdd0b1824ce70e13bb256
AsiyaBSainudeen <67639407+AsiyaBSainudeen@users.noreply.github.com>
2026-02-07 21:26:16 +0000

    Deploying to gh-pages from @ AsiyaBSainudeen/AsiyaBSainudeen.github.io@10385b2571064277baabff28b62fd672cc5fc536 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -434,7 +434,7 @@ ninja.data = [{
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
+          window.open("https://scholar.google.com/citations?user=IP2U28YAAAAJ", "_blank");
         },
       },{
         id: 'social-custom_social',
