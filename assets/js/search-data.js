2712ec7ab6c82520a0e55c74a388faaf87c10b17
AsiyaBSainudeen <67639407+AsiyaBSainudeen@users.noreply.github.com>
2026-02-28 22:25:34 +0000

    Deploying to gh-pages from @ AsiyaBSainudeen/AsiyaBSainudeen.github.io@c626c7090a282c010504ec3b4174fe92d122bcb9 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -413,21 +413,14 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%61%73%69%79%61@%61%72%69%7A%6F%6E%61.%65%64%75", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/feed.xml", "_blank");
+          window.open("https://www.linkedin.com/in/asiya-b-sainudeen", "_blank");
         },
       },{
         id: 'social-scholar',
@@ -441,7 +434,7 @@ ninja.data = [{
         title: 'Custom_social',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://geo.arizona.edu/person/asiya-badarunnisa-sainudeen", "_blank");
         },
       },{
       id: 'light-theme',
